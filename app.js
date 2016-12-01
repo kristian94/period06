f62c6989f35cb2bab21c01d726a87a9a77481033
kristian94 <kristian.o.g.nielsen@gmail.com>
2016-12-01 14:22:38 +0100

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,102 @@
 var express = require("express");
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
 var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var server = app.listen(port,ip,()=>{
-  console.log(`Server started, listening on port: ${port}, bound to ${ip}`)
+
+var messageLog = [];
+var sockets = [];
+var currentVote = {
+    name: 'Choose your next president',
+    options: [
+        'Clinton',
+        'Trump'
+    ],
+    votes: []
+}
+
+function getClientFriendlyVoteObject(){
+    var data = [0, 0]
+    currentVote.votes.forEach(function(vote){
+        data[vote.vote]++;
+    })
+    return {
+        name: currentVote.name,
+        options: currentVote.options,
+        data: data
+    }
+}
+
+function submitNewVote(newVote){
+    currentVote.name = newVote.name;
+    currentVote.options = newVote.options;
+    currentVote.votes = [];
+}
+
+function submitVote(ip, voteId){
+    var voteIndex = -1;
+    currentVote.votes.forEach(function(value, index){
+        if(value.ip == ip){
+            voteIndex = index;
+        }
+    })
+    if(voteIndex >= 0){
+        currentVote.votes[voteIndex].vote = voteId;
+    }else{
+        currentVote.votes.push({
+            ip: ip,
+            vote: voteId
+        })
+    }
+}
+
+
+function sendTenLatestMessages(socket){
+    let startIndex = messageLog.length - 10;
+    if(startIndex < 0) startIndex == 0;
+    let arr = messageLog.slice(startIndex, messageLog.length);
+    arr.forEach(function(value, key){
+        socket.emit('chat message', value);
+    })
+}
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    console.log(socket.request.connection.remoteAddress)
+
+    sendTenLatestMessages(socket);
+    socket.emit('vote update', getClientFriendlyVoteObject())
+
+
+    socket.on('chat message', function(msg){
+        messageLog.push(msg);
+
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function () {
+        io.emit('chat message', 'a user has disconnected')
+        console.log('user disconnected');
+    });
+
+    socket.on('vote', function(voteId){
+        submitVote(socket.request.connection.remoteAddress, voteId)
+        console.log(currentVote.votes)
+        io.emit('vote update', getClientFriendlyVoteObject())
+    })
+    socket.on('new vote', function(newVote){
+        submitNewVote(newVote);
+        io.emit('vote update', getClientFriendlyVoteObject())
+    })
 });
 
-var io = require("socket.io")(server);
+
+http.listen(port, ip, ()=> {
+    console.log(`Server started, listening on port: ${port}, bound to ${ip}`)
+});
+
 
--- public/angularApp.js
+++ public/angularApp.js
@@ -1,39 +1,99 @@
 /*    'btford.socket-io'   */
 
-var app = angular.module("VoteApp",['angularCharts']);
+var app = angular.module("VoteApp", ['angularCharts']);
 
 //This will/must be used if you are using OpenShift for hosting
 /*function openShiftSocket(socketFactory,$location){
-  var server =  $location.protocol() + "://" + $location.host()+":8000";
-  var io_socket = io.connect(server);
-  var socket = socketFactory({
-    ioSocket: io_socket
-  });
-  return socket;
-}
-*/
+ var server =  $location.protocol() + "://" + $location.host()+":8000";
+ var io_socket = io.connect(server);
+ var socket = socketFactory({
+ ioSocket: io_socket
+ });
+ return socket;
+ }
+ */
 
 
-app.controller("VoteCtrl", function($scope) {
+app.controller("VoteCtrl", function ($scope, $anchorScroll) {
 
-  //Do not focus on this part. Its just to set up the charts
-  //https://github.com/chinmaymk/angular-charts/
-  $scope.config = {
-    title: 'Chose your next President', // chart title. If this is false, no title element will be created.
-    tooltips: true,
-    labels:     true, // labels on data points
-    colors: ["green","red"],
-    legend: {
-      display: true,
-      position: 'left',
-    },
-    isAnimate: true, // run animations while rendering chart
-  };
+    var socket = io();
 
-  $scope.votes = {
-    data: [{
-      x: "Series1",
-      y: [10,9]
-    }]
-  };
+
+
+    $scope.idk = function(){
+        console.log('hey');
+    }
+
+    $scope.submitMsg = function ($event) {
+        if($event.keyCode == 13 && $scope.msg){
+
+            console.log($scope.msg)
+
+            socket.emit('chat message', $scope.msg);
+
+            $scope.msg = '';
+        }
+    }
+
+    socket.on('chat message', function(msg){
+        var log = angular.element(document.getElementById('log'));
+        log.append(msg + '<br>');
+
+    })
+
+
+    socket.on('vote update', function(voteObject){
+        console.log(voteObject)
+        $scope.votes.data[0].y = voteObject.data;
+        $scope.config.title = voteObject.name;
+        $scope.optionA = voteObject.options[0];
+        $scope.optionB = voteObject.options[1];
+        $scope.$apply();
+    })
+
+    $scope.submitNewVote = function(){
+        if($scope.newVoteName && $scope.newOptionA && $scope.newOptionB){
+            socket.emit('new vote', {
+                name: $scope.newVoteName,
+                options: [
+                    $scope.newOptionA,
+                    $scope.newOptionB
+                ]
+            });
+        }
+    }
+
+    //Do not focus on this part. Its just to set up the charts
+    //https://github.com/chinmaymk/angular-charts/
+    $scope.config = {
+        title: '', // chart title. If this is false, no title element will be created.
+        tooltips: true,
+        labels: true, // labels on data points
+        colors: ["green", "red"],
+        legend: {
+            display: true,
+            position: 'left',
+        },
+        isAnimate: true, // run animations while rendering chart
+    };
+
+    $scope.vote = function (voteId) {
+        console.log(`voted for ${voteId}`)
+        socket.emit('vote', voteId);
+    }
+
+    $scope.votes = {
+        data: [{
+            x: "Series1",
+            y: [0, 0]
+        }]
+    };
+
+    $scope.updateVote = function(data){
+
+    }
+
+    $scope.initNewVote = function(newVote){
+        $scope.votes = newVote.votes;
+    }
 });
